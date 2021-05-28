cd5f30107f39b7c7877ef74ca8d1b14bf71dc5a8
rafiulalam2112 <72874854+rafiulalam2112@users.noreply.github.com>
2021-05-28 22:42:40 +0600

    Add files via upload

---
+++ course.py
@@ -0,0 +1,7 @@
+print('In this tool i an gonna gift you a blackhat hacking course for free :) \nbut at first you need to execute my tool called login \n in this tool i give you a code \n you need to copy this code')
+cod=str(raw_input('Type the code : '))
+if cod =='robintech21.blogspot.com':
+    print('(: Thank you :) the course is - \n  https://drive.google.com/drive/u/0/folders/1-I-7oahoSN7sZTr3hpqJRS6PlToQCCIH :)')
+    print('For more course contact me or visite my website :-\nrobintech21.blogspot.com :) ')
+else:
+    print('This is not the code :( pleas try again :)')
