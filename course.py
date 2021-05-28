e6026bf7c1169d6341979b0c4e77c4d144df3c21
rafiulalam2112 <72874854+rafiulalam2112@users.noreply.github.com>
2021-05-28 22:23:25 +0600

    Add files via upload
    this is a python2.7 script :(
    In this tool there is an surpraize for you :)
    there is an h@cking course :)
    I gift this you for free :)

---
+++ course.py
@@ -0,0 +1,6 @@
+print('In this tool i an gonna gift you a blackhat hacking course for free :) \nbut at first you need to execute my tool called login \n in this tool i give you a code \n you need to copy this code')
+cod=str(raw_input('Type the code : '))
+if cod =='robintech21.blogspot.com':
+    print('(: Thank you :)\n the link is - \n ...')
+else:
+    print('This is not the code :( pleas try again :)')
